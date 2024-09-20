67babfffdd4f0e49918e7004500952667756eb82
avinash <abiavinash443@gmail.com>
2024-09-20 19:46:10 +0530

    firstchanges

---
+++ frontend/src/store.js
@@ -0,0 +1,32 @@
+import {legacy_createStore as createStore, combineReducers, applyMiddleware, compose,} from "redux";
+import thunk from "redux-thunk";
+import { restaurantReducer } from "./reducer/restaurantReducer";
+import { menuReducer } from "./reducer/menuReducer";
+import {authReducer, forgotPasswordReducer, userReducer} from  "./reducer/userReducer";
+import { cartReducer } from "./reducer/cartReducer";
+import { myOrdersReducer, newOrderReducer, orderDetailsReducer,  } from "./reducer/orderReducer";
+
+
+const reducer = combineReducers({
+    restaurants:  restaurantReducer,          //all the datas will return to this restaurants in resturantreducer datas
+    menus:  menuReducer,
+    auth : authReducer,
+    user : userReducer,
+    forgotPassword:  forgotPasswordReducer,
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer,
+
+});
+
+const composeenhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware =  [ thunk];
+
+const store = createStore(
+    reducer,
+    composeenhancers(applyMiddleware(...middleware))
+);
+
+export default store;
